3df6e7accd76c9c601faa829eb93dce86650a34f
gabrieleprovenzano <146100399+gabrieleprovenzano@users.noreply.github.com>
2023-11-15 12:41:10 +0100

    Add files via upload

--- escallback1.js
+++ escallback1.js
@@ -1,14 +1,14 @@
 function printAsyncName(callback, name) {
-
-    setTimeout(function() {
-      console.log('Hello');
-      
-      setTimeout(function() {
-        console.log(name);
-      }, 1000);
+  setTimeout(function () {
+    callback();
+    setTimeout(function () {
+      console.log(name);
     }, 1000);
-  }
+  }, 1000);
+}
 
-  printAsyncName(function() {
-    console.log('Callback executed');
-  }, 'John');
+function sayHello() {
+  console.log("Hello");
+}
+
+printAsyncName(sayHello, "Gabriele Provenzano");
